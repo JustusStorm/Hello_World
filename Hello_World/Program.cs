542fefc5fb1781d721967bf0eec0aa21541c0771
Justus Storm <storm.j.dev@gmail.com>
2020-08-19 20:27:20 -0500

    Added user input to start program

--- Hello_World/Program.cs
+++ Hello_World/Program.cs
@@ -6,7 +6,32 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\n");
+            // Talk to the user, ask them if they are ready to code
+            // take their input
+            // if input is "yes", continue program, if not, then exit code
+
+            Console.WriteLine("Hello user, are you ready to try some coding? yes/no");
+            Console.Write("> ");
+            string userAnswer = Console.ReadLine().ToLower().Trim();
+            char firstInputIndex = userAnswer[0];
+
+
+            if (firstInputIndex == 'y')
+            {
+                Console.WriteLine("Here we go then.\n");
+            }
+            else
+            {
+                Console.WriteLine("You aren't ready. Come back later when you're ready to become a developer.");
+            }
+
+            Console.WriteLine("");
+
+
+
+
+
         }
     }
 }
